51e4eb9f6f2e18cb8a3ad48288c2582df1794295
autominds <automindhq@gmail.com>
2026-04-24 19:31:46 +0530

    chore: inject Google Tag Manager scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,10 +113,27 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVTNKW3W');`,
+          }}
+        />
         <meta name="google-site-verification" content="iG4MsXnd6nBL_s7Z3DEJZ_Rz67imbAL-eVYmBmHXpVg" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
       </head>
       <body className="flex min-h-screen flex-col overflow-x-hidden bg-background font-sans text-foreground">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KVTNKW3W"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <AppShell>{children}</AppShell>
         <Script src="/chatbot.js" strategy="lazyOnload" />
       </body>
